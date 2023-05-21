0b4952630436433a43e7f6439316450437188254
asliddinberdiyevv <asliddinberdiyevv@gmail.com>
2023-05-21 11:07:32 +0500

    testing deploy

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ func main() {
 	server := gin.New()
 	flag.Parse()
 	logrus.SetLevel(logrus.DebugLevel)
-	logrus.WithField("port", os.Getenv("APP_PORT")).Debug("starting server")
+	logrus.WithField("port", os.Getenv("PORT")).Debug("starting server")
 
 	server.GET("/", func(ctx *gin.Context) {
 		logrus.Info("get request")
 		ctx.JSON(http.StatusOK, gin.H{"message": "main server"})
 	})
 
-	if err := server.Run(os.Getenv("APP_HOST") + ":" + os.Getenv("APP_PORT")); err != nil {
+	if err := server.Run(":" + os.Getenv("PORT")); err != nil {
 		logrus.WithError(err).Fatal("Error starting server")
 	}
 }
