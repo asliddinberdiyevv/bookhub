8f5c2c4620191776eb511298dc5c1db5fad202ce
asliddinberdiyevv <asliddinberdiyevv@gmail.com>
2023-05-31 18:56:47 +0500

    add get handle

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bookhub/db"
 	"flag"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -21,6 +22,13 @@ func main() {
 
 	RS := db.ConnectRS()
 	defer RS.Close()
+
+	server.GET("/", func(ctx *gin.Context) {
+		logrus.Info("get request done")
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "successful",
+		})
+	})
 
 	if err := server.Run(":" + os.Getenv("PORT")); err != nil {
 		logrus.WithError(err).Fatal("Error starting server")
